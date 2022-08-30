6c71081d38f93007ddabffcea2f4a514f6e64a66
alceil <ashishshajisrampickal@gmail.com>
2022-08-30 22:17:37 +0530

    Add Authentication reducer

--- src/redux/actions/auth.action.js
+++ src/redux/actions/auth.action.js
@@ -4,4 +4,32 @@
 export const signup = (data) => ({
     type: 'SIGNUP_SUCCESS',
     payload: data,
+  }
+  );
+
+  export const loginmodalopen = () => ({
+    type: 'LOGIN_MODAL_OPEN',
   });
+  export const loginmodalclose = () => ({
+    type: 'LOGIN_MODAL_CLOSE',
+  });
+  export const signupmodalopen = () => ({
+    type: 'SIGNUP_MODAL_OPEN',
+  });
+  export const signupmodalclose = () => ({
+    type: 'SIGNUP_MODAL_CLOSE',
+  });
+
+  export const editprofilemodalopen = () => ({
+    type: 'EDIT_MODAL_OPEN',
+  });
+
+  export const editprofilemodalclose = () => ({
+    type: 'EDIT_MODAL_CLOSE',
+  });
+
+  export const updatedetails = (data) => ({
+    type: 'UPDATE_SUCCESS',
+    payload: data,
+  }
+  );
--- src/redux/reducers/auth.reducer.js
+++ src/redux/reducers/auth.reducer.js
@@ -1,19 +1,59 @@
 
 const initialState = {
-  user: {},
   isUserLoggedIn: false,
+  isEditModalVisible: false,
+  isLoginModalVisible: false,
+  isSignUpModalVisible: false,
+  user: {
+    firstname: null,
+  },
 };
 
 const AuthReducer = (state = initialState, action) => {
-  console.log(action);
-  console.log(action.type);
+  console.log("DATA")
   switch (action.type) {
     
     case 'SIGNUP_SUCCESS':
       return {
         ...state,
+        user:action.payload,
         isUserLoggedIn: true,
       };
+      case 'UPDATE_SUCCESS':
+        return {
+          ...state,
+          user:action.payload,
+        };
+      case 'LOGIN_MODAL_OPEN':
+        return {
+          ...state,
+          isLoginModalVisible: true,
+        };
+        case 'SIGNUP_MODAL_OPEN':
+          return {
+            ...state,
+            isSignUpModalVisible: true,
+          };
+          case 'LOGIN_MODAL_CLOSE':
+            return {
+              ...state,
+              isLoginModalVisible: false,
+            };
+            case 'SIGNUP_MODAL_CLOSE':
+              return {
+                ...state,
+                isSignUpModalVisible: false,
+              };
+              case 'EDIT_MODAL_OPEN':
+                return {
+                  ...state,
+                  isEditModalVisible: true,
+                };
+                case 'EDIT_MODAL_CLOSE':
+                  return {
+                    ...state,
+                    isEditModalVisible: false,
+                  };
 
     default:
       return state;
