72ea1b6617c97c41838b620eb6767e98e878fc7f
alceil <ashishshajisrampickal@gmail.com>
2022-08-30 23:02:51 +0530

    Add DeleteAccount component

---
+++ src/components/pages/Settings/DeleteAccount/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './styles.css'
+import { MdDelete } from 'react-icons/md';
+import Button from '../../../common/Button'
+const DeleteAccount = () => {
+  return (
+    <div className='delete-account-card'>
+      <div className='delete-account-card-top'>
+<h3>Account deletion</h3>
+<p>Once you delete your account, there is no going back. Please be certain.</p>
+      </div>
+      <Button className='delete-account-btn'>
+        <div className='icon'>
+        <MdDelete />
+        </div>
+        <span>
+        Delete Account
+
+        </span>
+
+      </Button>
+    </div>
+  )
+}
+
+export default DeleteAccount
