5d03dade8bae607b7d33166b138ef03356943159
alceil <ashishshajisrampickal@gmail.com>
2022-11-10 15:30:10 +0530

    Add UserProfile

--- src/components/pages/Profile/UserProfile/index.jsx
+++ src/components/pages/Profile/UserProfile/index.jsx
@@ -1,41 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios";
 import Button from '../../../common/Button'
 import { CgLogOut } from 'react-icons/cg';
-import { useDispatch } from "react-redux"
 import IconButton from '../../../common/IconButton';
-import './styles.css'
+import style from './styles.module.css'
 import EditProfile from './EditProfile';
-import { useSelector } from "react-redux";
-import {  editprofilemodalopen } from '../../../../redux/actions/auth.action'
+import { useDispatch, useSelector } from "react-redux";
+import ImageUploadButton from '../../../common/ImageUploadButton';
+import { updateProfilePicService } from '../../../../redux/slices/users.slice';
+import placeholderProfilePicture from '../../../../assets/images/placeholder_profile_picture.png';
 const UserProfile = () => {
-  const dispatch = useDispatch();
-  const isEditModalVisible = useSelector((state) => state.isEditModalVisible);
+  const dispatch = useDispatch()
+  
+const [isModalOpen,setIsModalOpen] = useState(false)
+  const user = useSelector((state) => state.user.user);
+  const isUserLoggedIn = useSelector((state) => state.user.isUserLoggedIn);
+  const {firstname,bio,lastname,username,profilePic,_id} =user;
 
-  const user = useSelector((state) => state.user);
   const editHandler = () => {
     console.log("edit pressed")
-    dispatch(editprofilemodalopen());
-    console.log(isEditModalVisible)
+    setIsModalOpen(true)
+};
+
+const handlePictureUpload = (file) => {
+  getBase64(file)
+};
+
+const getBase64 = (file) => {
+  let reader = new FileReader();
+  reader.readAsDataURL(file);
+
+  reader.onload = () => {
+    const formData = new FormData();
+    formData.append(
+      "image",
+      reader.result.slice(file.type === "image/png" ? 22 : 23)
+    );
+    formData.append("name", file.name);
+    formData.append("key", process.env.REACT_APP_PUBLIC_IMGBB_STORAGE_KEY);
+    console.log("Key#####")
+    console.log(process.env.REACT_APP_PUBLIC_IMGBB_STORAGE_KEY)
+    const upload = axios
+      .post("https://api.imgbb.com/1/upload", formData)
+      .then((res) => {
+        console.log(res.data.data.url)
+        dispatch(updateProfilePicService({
+          id:_id,
+          imageUrl:res.data.data.url
+        }))
+        // setData({...data,imageUrl: res.data.data.url})
+        // console.log(data)
+      });
+  
+
+
+      // toast.promise(upload, {
+      //   loading: "Uploading...",
+      //   success: `Uploaded ${file.name}.png`,
+      //   error: "Error Uploading File",
+      // });
+  };
 };
   return (
 
     <>
-       <div className='user-profile-card'>
-    <div className='user-profile-card-top'>
+       <div className={style.user_profile_card}>
+    <div className={style.user_profile_card_top}>
         <img 
         src="https://source.unsplash.com/800x600/?nature" 
         alt="coverimage"
-        className='cover-image'
+        className={style.cover_image}
         />
     </div>
-<div className='user-profile-card-middle'>
-<img
-      class="profile-image"
-      src="https://polygram.herokuapp.com/api/pictures/6217439fb0b70e0016cc6b77"
+<div className={style.user_profile_card_middle}>
+  <div class={style.profile_image}>
+  <img
+      class={style.profile_picture}
+      src={isUserLoggedIn?profilePic:placeholderProfilePicture}
       alt="profilepic"
     />
-<div className='button-grp'>
-   <Button className={'edit-btn'} onClick={editHandler()}>
+    <ImageUploadButton
+                    accept="image/png, image/jpeg, image/jpg"
+                    className="profile-picture-upload"
+                    onUpload={handlePictureUpload}
+    />
+  </div>
+
+<div className={style.button_grp}>
+   <Button className={style.edit_btn} onClick={editHandler}>
     Edit Profile
     </Button> 
 
@@ -48,17 +100,20 @@ const UserProfile = () => {
 </div>
 
 </div>
-<div className='user-profile-card-bottom'>
-<h3 className='name'>
-{user.firstname??""}
+<div className={style.user_profile_card_bottom}>
+<h3 className={style.name}>
+{firstname??""}
+ {lastname && ` ${lastname}`}
 </h3>
-<span className='username'>
-    @alceil
+<p>
+  {bio}
+</p>
+<span className={style.username}>
+    @{username}
 </span>
 </div>
    </div>
-   <EditProfile/>
-
+   <EditProfile isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </>
 
   )
