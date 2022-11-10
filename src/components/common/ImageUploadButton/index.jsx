aa942963ad27d68c959dfa47ce866241f94200e2
alceil <ashishshajisrampickal@gmail.com>
2022-11-10 15:28:32 +0530

    Add ImageUpload component

---
+++ src/components/common/ImageUploadButton/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useRef } from 'react';
+import { GrEdit } from 'react-icons/gr';
+
+
+const ImageUploadButton = ({ className, onUpload, name, id, accept }) => {
+  const uploader = useRef(null);
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    onUpload(file);
+  };
+  return (
+    <div className={`file-upload ${className ?? ''}`}>
+    <input
+      id={id}
+      type="file"
+      name={name}
+      ref={uploader}
+      accept={accept}
+      onChange={handleFileChange}
+      style={{ display: 'none' }}
+    />
+    <div
+      className="upload-btn"
+      role="button"
+      tabIndex={0}
+      onClick={() => uploader?.current?.click()}
+    >
+      <GrEdit />
+    </div>
+  </div>
+  )
+}
+
+export default ImageUploadButton
