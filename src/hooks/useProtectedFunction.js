6210f95aa267ebfd6d2e7eb0f89b87783fd95dda
alceil <ashishshajisrampickal@gmail.com>
2023-04-13 22:26:33 +0530

    Add useProtected Function hook

---
+++ src/hooks/useProtectedFunction.js
@@ -0,0 +1,35 @@
+import { useCallback } from 'react';
+import { useSelector,useDispatch } from "react-redux";
+import {loginmodalopen } from '../../../redux/slices/users.slice';
+/**
+ *
+ * Custom hook which returns a function which modifies any function
+ * in such a way that only the logged in user can use it.
+ * @returns A function which modifies event handler functions
+ */
+const useProtectedFunction = () => {
+    const isUserLoggedIn = useSelector((state) => state.user.isUserLoggedIn);
+  const dispatch = useDispatch();
+
+  /**
+   *
+   * A Higher order function which modifies any function in such a way that
+   * only the logged in user can use it.
+   * @param functionToProtect The function to be protected from logged out user
+   * @returns Modified function which will only triggers when called by logged in user
+   */
+  const protectFunction = useCallback(
+    (functionToProtect) => (...args) => {
+      if (isUserLoggedIn) {
+        functionToProtect(...args);
+      } else {
+        dispatch(loginmodalopen());
+      }
+    },
+    [isUserLoggedIn]
+  );
+
+  return protectFunction;
+};
+
+export default useProtectedFunction;
