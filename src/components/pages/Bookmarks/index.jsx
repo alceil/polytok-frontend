315a913e630c44aff3201e21250135987db8c623
alceil <ashishshajisrampickal@gmail.com>
2023-05-08 18:47:48 +0530

    Added bookmark page

--- src/components/pages/Bookmarks/index.jsx
+++ src/components/pages/Bookmarks/index.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
-
+import './styles.css'
+import { FiArrowLeft } from 'react-icons/fi';
+import IconButton from '../../common/IconButton';
+import { useSelector } from "react-redux";
+import PostCard from '../../common/PostCard';
 const BookMarks = () => {
+  const user = useSelector((state) => state.user.user);
+  const {bookmarks} = user;
   return (
-    <div>No bookmarks left</div>
+    <div className='page'>
+        <div className='page-back'>
+        <IconButton className={'back-btn'}>
+            <FiArrowLeft/>
+        </IconButton>
+        <h2>Bookmarks</h2>
+    </div>
+    {bookmarks.length > 0 ? (
+        <div className='bookmarks'>
+          {bookmarks.map((bookmark, index) => (
+            <div key={bookmark._id}>
+            <PostCard 
+            postData={bookmark}
+  /> 
+              </div>
+          ))}
+        </div>
+      ) : (
+        <p style={{  textAlign: 'center'}}>No bookmarks found.</p>
+      )}
+    </div>
   )
 }
 
